553ca3319b77ec8b8aac10cb8a0f6c4dc86febdd
Austin Papritz <austin@papritz.dev>
2023-08-18 11:52:37 -0700

    added context service to Program.cs

--- NpBioApi/Models/NpBioApiContext.cs
+++ NpBioApi/Models/NpBioApiContext.cs
@@ -2,10 +2,10 @@ using Microsoft.EntityFrameworkCore;
 
 public class NpBioApiContext : DbContext
 {
+    public DbSet<Park> Parks { get; set; }
+    public DbSet<Species> Species { get; set; }
     public NpBioApiContext(DbContextOptions<NpBioApiContext> options) : base(options)
     {
     }
 
-    public DbSet<Park> Parks { get; set; }
-    public DbSet<Species> Species { get; set; }
 }
